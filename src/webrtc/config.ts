a20a4b6a42a7015202ef4ddf8eef3ac9aa3a04dd
Raphael <raphael.hauk@racing.tuwien.ac.at>
2019-11-03 18:04:16 +0100

    update turn uri

--- src/webrtc/config.ts
+++ src/webrtc/config.ts
@@ -1,2 +1,2 @@
-export const iceConfig =  { iceServers: [{"urls":["turn:hub.greenchat.me:3478"],"username":"raphael","credential":"@123@test@"}] };
+export const iceConfig =  { iceServers: [{"urls":["turn:turn.greenchat.me:443"],"username":"raphael","credential":"@123@test@"}] };
 export const signallingServer = "wss://hub.greenchat.me";
